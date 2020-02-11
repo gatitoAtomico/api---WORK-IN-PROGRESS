1c7edd68656f79d0beb51a5e1d6258574b82861b
gatitoAtomico <sup.georgios@gmail.com>
2020-02-12 00:13:00 +0200

    migrations created for products and reviews

--- database/migrations/2020_02_10_224526_create_reviews_table.php
+++ database/migrations/2020_02_10_224526_create_reviews_table.php
@@ -15,6 +15,12 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('product_id')->unsigned()->index();
+            //set the foreign key with the products table, if the corresponding parent deleted in the table then the child's in this table will also be deleted
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
